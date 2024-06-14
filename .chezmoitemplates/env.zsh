d0285db762c3155579f92509328ca6dc67e1a74f
Mikołaj Rosowski <m.rosowski1@wp.pl>
2024-06-14 22:51:45 +0200

    chezmoi setup
    Signed-off-by: Mikołaj Rosowski <m.rosowski1@wp.pl>

---
+++ .chezmoitemplates/env.zsh
@@ -0,0 +1,76 @@
+# XDG Base
+export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
+export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
+export XDG_OPT_HOME="${XDG_OPT_HOME:-$HOME/.local/opt}"
+export XDG_SRC_HOME="${XDG_SRC_HOME:-$HOME/.local/src}"
+export XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME/.local/state}"
+
+# Apptainer stuff
+export APPTAINER_CACHEDIR="$XDG_CACHE_HOME/apptainer"
+export APPTAINER_CONTAINERS="$XDG_SRC_HOME/apptainer"
+export APPTAINER_BINDPATH="${WAYLAND_DISPLAY:+${XDG_RUNTIME_DIR}/${WAYLAND_DISPLAY},}$SSH_AUTH_SOCK"
+
+# Cmake stuff
+export CMAKE_EXPORT_COMPILE_COMMANDS="Yes"
+export CMAKE_GENERATOR="Ninja"
+
+export COMPILERS_BASE="$XDG_OPT_HOME/compilers"
+export CROSS_BASE="$COMPILERS_BASE/cross"
+
+# Go stuff
+export GOPATH="$XDG_DATA_HOME/go"
+{{- if eq .chezmoi.osRelease.id "debian" }}
+export GOHOME="$COMPILERS_BASE/go"
+{{- end }}
+
+# Python stuff
+export WORKON_HOME="$XDG_DATA_HOME/virtualenv"
+
+# Rust stuff
+export CARGO_HOME="$XDG_DATA_HOME/cargo"
+export RUSTUP_HOME="$XDG_DATA_HOME/rustup"
+
+# Zig stuff
+export ZIG_HOME="$COMPILERS_BASE/zig"
+
+# Zephyr stuff
+export ZEPHYR_WORKSPACE="$XDG_OPT_HOME/zephyrproject"
+export ZEPHYR_WEST="$ZEPHYR_WORKSPACE/west"
+export ZEPHYR_BASE="$ZEPHYR_WORKSPACE/zephyr"
+export ZEPHYR_SDK_INSTALL_DIR="$ZEPHYR_WORKSPACE/sdks"
+
+# Defaults
+export EDITOR=nvim
+{{- if eq .chezmoi.osRelease.id "debian" }}
+export PAGER=batcat
+{{- else }}
+export PAGER=bat
+{{- end }}
+
+# Cleanup
+export HISTFILE="$XDG_DATA_HOME/zsh_history"
+export ZDOTDIR="$XDG_CONFIG_HOME/zsh"
+export LESSHISTFILE=-
+export DOCKER_CONFIG="$XDG_CONFIG_HOME/docker"
+
+# PATH helpers
+prepend_path() {
+  [ -d "$1" ] && PATH="$1:$PATH"
+}
+append_path() {
+  [ -d "$1" ] && PATH="$PATH:$1"
+}
+
+append_path "$HOME/.bin"
+append_path "$HOME/.local/bin"
+
+{{- if eq .chezmoi.osRelease.id "debian" }}
+append_path "$RUSTUP_HOME/bin"
+append_path "$GOHOME/bin"
+{{ end -}}
+append_path "$CARGO_HOME/bin"
+append_path "$GOPATH/bin"
+append_path "$ZIG_HOME"
+
+export PATH
--- README.md
+++
@@ -1,8 +0,0 @@
-# Dotfiles
-My dotfiles
-
-### Base
-Contains config for apps that I use everywhere
-
-### Hyprland
-Contains config files for Hyprland & all other apps that I use with it
