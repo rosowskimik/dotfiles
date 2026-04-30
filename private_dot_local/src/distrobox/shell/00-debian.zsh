95118fbf2674ce28ded0196a29ecb242f5284089
Mikołaj Rosowski <m.rosowski1@wp.pl>
2026-04-30 23:12:27 +0200

    Update .local/src/distrobox/shell/00-debian.zsh

--- private_dot_local/src/distrobox/shell/00-debian.zsh
+++ private_dot_local/src/distrobox/shell/00-debian.zsh
@@ -7,4 +7,6 @@ alias ls="eza --color=auto --group-directories-first -F"
 alias l="ls --smart-group -lgahM"
 alias lzg="lazygit"
 
+export TERM=kitty
+
 sudo systemd-run --scope --no-block --quiet -- apt update -qqy
