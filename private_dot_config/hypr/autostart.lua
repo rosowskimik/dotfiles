57333135c5ff45778b4faaadd19ff0fb99e319d4
Mikołaj Rosowski <m.rosowski1@wp.pl>
2026-07-28 13:33:37 +0200

    Update .config/hypr/autostart.lua

--- private_dot_config/hypr/autostart.lua
+++ private_dot_config/hypr/autostart.lua
@@ -3,7 +3,7 @@
 -------------------
 
 hl.on("hyprland.start", function()
-	hl.exec_cmd("cliphist-wipe")
+	hl.exec_cmd("cliphist wipe")
 
 	hl.exec_cmd("uwsm app -s b -- wl-paste --type text --watch cliphist store")
 	hl.exec_cmd("uwsm app -s b -- wl-paste --type image --watch cliphist store")
